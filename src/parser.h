a40d7852912021991bb19fd2aa90e0550cd8caba
Priklen <igorsamodumskij@gmail.com>
2026-08-06 23:58:17 +0300

    parser header

---
+++ src/parser.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "lexer.h"
+
+#define INFIX_TYPE \
+	X(ASSIGN) \
+	X(ADD) \
+	X(SUB) \
+	X(DIV) \
+	X(MUL) \
+	X(POW) \
+
+typedef enum {
+#define X(name) INFIX_##name,
+	INFIX_TYPE
+#undef X
+} InfixType;
+
+#define NODE_TYPE \
+	X(INFIX) \
+	X(LET) \
+
+typedef enum {
+#define X(name) NODE_##name,
+	NODE_TYPE
+#undef X
+} NodeType;
+
+typedef struct Node {
+	NodeType type;
+	union {
+		struct {
+			struct Node *left, *right;
+			InfixType type;
+		} infix;
+		struct {
+			struct Node *assign;
+			bool mut;
+		} let;
+	};
+} Node;
+
+void Node_print(const Node *);
+void Node_free(const Node *);
+Node *parse(TokenStream tokens);
